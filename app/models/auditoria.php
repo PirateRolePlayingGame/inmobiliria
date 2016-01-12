94b5f085ce3bca03ba77933884e2c5593fece36c
Samuel Alvarez <seam.2194@gmail.com>
2016-01-12 01:55:36 -0430

    modelo auditoria

---
+++ app/models/auditoria.php
@@ -0,0 +1,64 @@
+<?php
+class auditoria
+{
+	public $actividad;
+	public $usuario;
+	public $fecha;
+	public $codigo;
+
+
+	public function __construct($act, $us, $fec, $cod)
+	{
+		$this->actividad = $act;
+		$this->usuario = $us;
+		$this->fecha = $fec;
+		$this->codigo = $cod; 
+	}
+
+	public static function obtAuditorias()
+	{
+		$V = [];
+		$db = Db::getInstance();
+		$req = $db->query('SELECT * from auditoria');
+
+		foreach($req->fetchAll() as $a)
+		{
+			$V[] = new auditoria($a['actividad'], $a['usuario'], $a['fecha'], $a['codigo']);
+		}
+		return $V;
+	}
+
+	public static function agrAuditoria($idu, $idinm, $var)
+	{
+		$db = Db::getInstance();
+		$this->{$var}($idu, $idinm, $i = 0, $viejo = 0)    //cuidado
+	}
+
+	public static function agrInmueble($idu, $idinm)
+	{
+		$db = Db::getInstance();
+		$fecha = date('Y/m/d');
+		$db->query('INSERT into auditoria(idUsuario, idInmueble, actividad, fecha) Values('$idu', '$idinm', "Agregó" '$fecha')');
+
+	}
+
+	public static function modInmueble($idu, $idinm, $i, $viejo) //Al modificar, se debe buscar el # relacionado con la columna que se modifico. Ese es $i.
+	{
+		$db = Db::getInstance();
+		$fecha = date('Y/m/d');
+		$inf = $db->query('SHOW COLUMNS from inmueble')
+		$inf->fetchAll();
+		$db->query('INSERT into auditoria(idUsuario, idInmueble, actividad, fecha) Values('$idu', '$idinm', "Modificó el '$i' de '$viejo'", '$fecha')');
+		//Falta terminar de formatear.
+	}
+
+	public static function inhInmueble($idu, $idinm)
+	{
+		$db = Db::getInstance();
+		$fecha = date('Y/m/d');
+		$db->query('INSERT into auditoria(idUsuario, idInmueble, actividad, fecha) Values('$idu', '$idinm', "Inhabilitó" '$fecha')');
+
+	}
+
+} 
+?>
