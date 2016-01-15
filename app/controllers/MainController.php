4041e8bf532ed9ff17e1a3b466fc5c66b8919f8c
victor <victormtortolero@gmail.com>
2016-01-14 21:31:50 -0430

    Fixed .htaccess

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,6 +3,8 @@
 class MainController{
 	public function main(){
 		require_once('/views/' . GC::$lang . "/bootstrapDashboardTemplate/index.html");
+		// var_dump(GC::$controller);
+		// var_dump(GC::$action);
 	}
 }
 
