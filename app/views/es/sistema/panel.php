e874a40a3abde3fa41bb6230cf4099801b278e09
victor <victormtortolero@gmail.com>
2016-01-15 00:20:04 -0430

    Adding Views

--- app/views/es/sistema/panel.php
+++ app/views/es/sistema/panel.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<base href="assets/">
 	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
 <body>
 	<div class="container-fluid">
@@ -18,49 +20,55 @@
 
 
 
+	
 
-	<script src="js/jquery-1.11.3.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script type="text/javascript">
-	$(document).ready(function(){
-		$("a.eliminar_dato").click( function(e) {
-			e.preventDefault();
-			var url = this.href;
-			var titulo = this.title;
-			jConfirm(titulo,'Accion !!', function(r) {
-				if (r) 
-					location.href = url;
+	
+		<!-- Scripts -->
+		<script type="text/javascript" language="javascript" src="js/jquery.dataTables.min.js"></script>
+		<script type="text/javascript" language="javascript" src="js/dataTables.bootstrap.js"></script>
+		
+		<script src="https:://code.jquery.com/jquery-1.12.0.min.js"></script>
+		<script src="https:://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		
+		<script type="text/javascript">
+			$(document).ready(function(){
+				$("a.eliminar_dato").click( function(e) {
+					e.preventDefault();
+					var url = this.href;
+					var titulo = this.title;
+					jConfirm(titulo,'Accion !!', function(r) {
+						if (r) 
+						location.href = url;
+					});
 				});
 			});
-	});
-	</script>
+		</script>
 
-   <script type="text/javascript" language="javascript" src="js/jquery.dataTables.min.js"></script>
-		<script type="text/javascript" language="javascript" src="js/dataTables.bootstrap.js"></script>
 		<script type="text/javascript" charset="utf-8">
-			$(document).ready(function() {
-				 $('#example').dataTable( {
+			$(document).ready(function(){
+				 $('#example').dataTable({
 					 //ordering: false, me elimina el ordenamiento y la funcion de ordenar al darle clicl a los encabezados
 					   "order": [[ 0, "desc" ]],
 					"language": {
-					"sProcessing":   "Procesando...",
-					"sLengthMenu":   "Mostrar _MENU_ registros",
-					"sZeroRecords":  "No se encontraron resultados",
-					"sInfo":         "Mostrando desde _START_ hasta _END_ de _TOTAL_ registros",
-					"sInfoEmpty":    "Mostrando desde 0 hasta 0 de 0 registros",
-					"sInfoFiltered": "(filtrado de _MAX_ registros en total)",
-					"sInfoPostFix":  "",
-					"sSearch":       "Buscar:",
-					"sUrl":          "",
-					"oPaginate": {
-						"sFirst":    "Primero",
-						"sPrevious": "Anterior",
-						"sNext":     "Siguiente",
-						"sLast":     "Último"
-	}
-}
-	} );
-} );
+						"sProcessing":   "Procesando...",
+						"sLengthMenu":   "Mostrar _MENU_ registros",
+						"sZeroRecords":  "No se encontraron resultados",
+						"sInfo":         "Mostrando desde _START_ hasta _END_ de _TOTAL_ registros",
+						"sInfoEmpty":    "Mostrando desde 0 hasta 0 de 0 registros",
+						"sInfoFiltered": "(filtrado de _MAX_ registros en total)",
+						"sInfoPostFix":  "",
+						"sSearch":       "Buscar:",
+						"sUrl":          "",
+						"oPaginate": {
+							"sFirst":    "Primero",
+							"sPrevious": "Anterior",
+							"sNext":     "Siguiente",
+							"sLast":     "Último"
+						}
+					}
+				});
+			});
 		</script>
 </body>
 </html>
